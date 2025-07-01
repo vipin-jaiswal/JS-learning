242c9278544cd078b4bb449261dca3ca94134b44
vipinjaiswal <166519126+vipin-jaiswal@users.noreply.github.com>
2025-07-01 11:58:22 +0000

    datatype summary

---
+++ js_with_chai/datatype-summary.js
@@ -0,0 +1,34 @@
+// primitive
+// 7 type  :  String, Number, Boolea, undefind, symbol, Bigint
+
+const score =100
+const scorevalue=100.3
+
+const isloggedI =false
+const outsideTemp =null
+let userEmail;
+const id =Symbol('123')
+const anotherId =Symbol('123')
+
+console.log(id===anotherId)
+
+const bigNumberb=32469785234n
+
+
+
+// Non primitive reference type  : array, Objects, Function
+   
+  const heros =["saktiman","naagraj","doga"]
+  let myobj={
+    name:"vipin",
+    age:22,
+    sub:"math"
+  }
+//   console.log(myobj)
+
+
+const myfunction=function(){
+    console.log("hello world");
+}
+myfunction()
+
