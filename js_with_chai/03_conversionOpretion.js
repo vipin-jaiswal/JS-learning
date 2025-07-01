532d681aa2e38bab7be878e6ac8b8914a07e800a
vipinjaiswal <166519126+vipin-jaiswal@users.noreply.github.com>
2025-07-01 09:52:44 +0000

    conversion of datatypes

---
+++ js_with_chai/03_conversionOpretion.js
@@ -0,0 +1,30 @@
+let score ="33"
+
+// const {score}=req.body
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+
+/*
+"33"=>33
+"33abc"=>NaN
+true=>1,false=>0
+*/
+
+let isloggedIn=1
+let booleanIsLoggedIn=Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// 1=>true 0=>false 
+// ""=>false
+// "vipin"=>true
+
+
+let someNumber =33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
