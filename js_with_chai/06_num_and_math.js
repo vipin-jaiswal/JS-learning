171eca2148940a319143076911f0551f922109eb
vipinjaiswal <166519126+vipin-jaiswal@users.noreply.github.com>
2025-09-24 14:50:23 +0000

    Number and math function

---
+++ js_with_chai/06_num_and_math.js
@@ -0,0 +1,35 @@
+const score =400
+// console.log(score)
+
+const  balance =new Number (100)
+// console.log(balance)
+
+// console.log(balance.toFixed(2))
+// console.log (balance.toString().length)
+
+const orthernumber =124.88
+
+// console.log(orthernumber.toPrecision(4))
+
+const hunderds= 1000000
+// console.log(hunderds.toLocaleString('en-IN'))
+
+
+// ++++++++++++++++++++++++++++++ MATHs +++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-1000))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.6))
+// console.log(Math.floor(4.6))
+// console.log(Math.min(4,6,3,5))
+// console.log(Math.max(4,6,3,5))
+
+
+// console.log(Math.floor((Math.random()*10)+1)) 
+
+//  if we want to pick rundum number between 10 to 20
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
