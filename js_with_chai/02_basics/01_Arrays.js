5b71b8e7d01ca5100b9b4e10180a16808df4d4b9
vipinjaiswal <166519126+vipin-jaiswal@users.noreply.github.com>
2025-10-07 15:19:45 +0000

    arr learning

--- js_with_chai/02_basics/01_Arrays.js
+++ js_with_chai/02_basics/01_Arrays.js
@@ -1,34 +1,34 @@
 // Array//
 
-// const myArr = [10,4,6,100,11,20,99,18,45,68,69]
+const myArr = [10,4,6,100,11,20,99,18,45,68,69]
 
-// console.log(myArr[10])
+console.log(myArr[10])
 
 
 const myarr2= new Array(0,1,2,3,4,5)
-// console.log(myarr2)
-// myarr2.push(7,8)
-// myarr2.pop()
+console.log(myarr2)
+myarr2.push(7,8)
+myarr2.pop()
 
 
-// // WHen you use shift that mean its remove the value of zeroth index
-// myarr2.shift()
+// WHen you use shift that mean its remove the value of zeroth index
+myarr2.shift()
 
-// // when you use unshift than its mean its add a value in the zeroth index which is pass in the brecates and on zeroth index value shift to +1index  
-// myarr2.unshift(5)
-// console.log(myarr2)
+// when you use unshift than its mean its add a value in the zeroth index which is pass in the brecates and on zeroth index value shift to +1index  
+myarr2.unshift(5)
+console.log(myarr2)
 
-// console.log(myarr2.lastIndexOf(1))
+console.log(myarr2.lastIndexOf(1))
 
-// // the indexof is work on the basis the index of first target value
-// console.log(myarr2.indexOf(1))
+// the indexof is work on the basis the index of first target value
+console.log(myarr2.indexOf(1))
 
-// // its give ans in true or false
-// console.log(myarr2.includes(4))
+// its give ans in true or false
+console.log(myarr2.includes(4))
 
-// const newarr= myarr2.join()
-// console.log(myarr2)
-// console.log(newarr)
+const newarr= myarr2.join()
+console.log(myarr2)
+console.log(newarr)
 
 //slice and splice
 
@@ -45,5 +45,6 @@ const myn2=myarr2.splice(1,3)
 
 console.log(myn2)
 console.log("B",myarr2)
+
 
 
