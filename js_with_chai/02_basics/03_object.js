6d46cefbc845bac701ac9af53813981a63f46ded
vipinjaiswal <166519126+vipin-jaiswal@users.noreply.github.com>
2025-10-08 13:15:23 +0000

    OBJECT

---
+++ js_with_chai/02_basics/03_object.js
@@ -0,0 +1,18 @@
+// singleton
+// Object.create
+
+//object literals
+
+const jsuser= {
+    name:"vipin",
+    "full name":"vipin jaiswal",
+    age:21,
+    location: "bhilai",
+    email: "vipin78040@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays:["mondaay","saturaday"]
+}
+
+console.log(jsuser.email)
+console.log(jsuser["email"])
+console.log(jsuser["full name"])
